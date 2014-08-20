d47e49a7b86f47d03985086e748b8951b4ca4c62
Mike Jackson <michaelj@epcc.ed.ac.uk>
2014-08-20 15:17:17 +0100

    Added comments on what each command does

--- wordcount.sh
+++ wordcount.sh
@@ -6,10 +6,36 @@
 
 # Usage: wordcount.sh < FILE [N]
 
-#  Bentley, Knuth, McIlroy 1986) Programming pearls: a literate
+# Bentley, Knuth, McIlroy 1986) Programming pearls: a literate
 # program Communications of the ACM, 29(6), pp471-483, June
 # 1986. http://dx.doi.org/10.1145/5948.315654. 
 # Dr. Drang, More shell, less egg, December 4th, 2011.,
 # http://www.leancrew.com/all-this/2011/12/more-shell-less-egg/ 
 
 tr -cs A-Za-z '\n' | tr A-Z a-z | sort | uniq -c | sort -rn | sed ${1}q
+
+# tr -cs A-Za-z '\n'
+# Translate all characters that are not A-Z or a-z to '\n'.
+# -c use the complement of A-Za-z '\n'.
+# -s squeeze together repeated occurrences of '\n' with a single '\n'.
+
+# tr A-Z a-z
+# Translate all characters that are A-Z to a-z.
+
+# sort
+# Sort standard input and write to standard output.
+
+# uniq -c 
+# Filter adjacent matching lines from standard input writing to
+# standard output.
+# -c prefix lines by the number of occurrences
+
+# sort -rn 
+# Sort standard input and write to standard output.
+# -r reverse the result of comparisons.
+# -n compare according to string numerical value.
+
+# sed ${1}q
+# Run stream editor and take first argument to shell script, N,
+# and slice from stream then quit.
+
