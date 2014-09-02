2463afcc6d572c8e56a4104432fd84380e45c0da
mikej888 <michaelj@epcc.ed.ac.uk>
2014-09-02 16:58:23 +0100

    Replaced for in with while loops

--- jacobi.py
+++ jacobi.py
@@ -13,16 +13,25 @@ def jacobi(niter, psi):
     tmp = [[0 for col in range(m+2)] for row in range(n+2)]
 
     # Iterate for number of iterations
-    for iter in range(niter):
+    iter = 0
+    while iter < niter:
 
         # Loop over the elements computing the stream function
-        for i in range(1,m+1):
-            for j in range(1,n+1):
+        i = 1
+        while i < m + 1:
+            j = 1
+            while j < n + 1:
                 tmp[i][j] = 0.25 * (psi[i+1][j]+psi[i-1][j]+psi[i][j+1]+psi[i][j-1])
+                j +=1
+            i +=1
 
         # Update psi
-        for i in range(1,m+1):
-            for j in range(1,n+1):
+        i = 1
+        while i < m + 1:
+            j = 1
+            while j < n + 1:
                 psi[i][j] = tmp[i][j]
+                j += 1
+            i += 1
 
-
+        iter += 1
